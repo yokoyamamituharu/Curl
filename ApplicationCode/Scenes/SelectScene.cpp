b12fb8078f7a502c1c03e0c9b26e0bb1e3ef30f7
Higa_0026 <k020g1382@g.neec.ac.jp>
2023-08-02 12:52:13 +0900

    easeOutBounce実装

--- ApplicationCode/Scenes/SelectScene.cpp
+++ ApplicationCode/Scenes/SelectScene.cpp
@@ -3,7 +3,7 @@
 #include "ImageManager.h"
 #include "Easing.h"
 
-const float SelectScene::easingTime = 40.0f;
+const float SelectScene::easingTime = 25.0f;
 
 void SelectScene::Initialize()
 {
@@ -30,9 +30,9 @@ void SelectScene::Update()
 	const float stage1LastPos = 400.0f;
 	const float stageSelectWindowLastPos = 360.0f;
 
-	tutorial_->SetPos({ 640.0f, Easing::easeOut(easingTimer_ , easingTime, tutorialLastPos, tutorial_->GetPos().y) });
-	stage1_->SetPos({ 640.0f, Easing::easeOut(easingTimer_, easingTime, stage1LastPos, stage1_->GetPos().y) });
-	stageSelectWindow_->SetPosition({ 640.0f, Easing::easeOut(easingTimer_, easingTime, stageSelectWindowLastPos, stageSelectWindow_->GetPosition().y) });
+	tutorial_->SetPos({ 640.0f, Easing::easeOutBounce(easingTimer_ , easingTime, tutorial_->GetPos().y, tutorialLastPos) });
+	stage1_->SetPos({ 640.0f, Easing::easeOutBounce(easingTimer_, easingTime, stage1_->GetPos().y, stage1LastPos) });
+	stageSelectWindow_->SetPosition({ 640.0f, Easing::easeOutBounce(easingTimer_, easingTime, stageSelectWindow_->GetPosition().y, stageSelectWindowLastPos) });
 	tutorial_->Update();
 	stage1_->Update();
 
--- Engine/Base/Easing.h
+++ Engine/Base/Easing.h
@@ -7,21 +7,21 @@ private:
 	~Easing() = default;
 
 public: //ÉÅÉìÉoä÷êî
-	static inline float easeIn(float time, float maxTime, float maxPos, float nowPos) {
+	static inline float easeIn(float time, float maxTime, float nowPos, float maxPos) {
 		maxPos -= nowPos;
 		time /= maxTime;
 
 		return maxPos * time * time + nowPos;
 	}
 
-	static inline float easeOut(float time, float maxTime, float maxPos, float nowPos) {
+	static inline float easeOut(float time, float maxTime, float nowPos, float maxPos) {
 		maxPos -= nowPos;
 		time /= maxTime;
 
 		return -maxPos * time * (time - 2.0f) + nowPos;
 	}
 
-	static inline float easeInOut(float time, float maxTime, float maxPos, float nowPos) {
+	static inline float easeInOut(float time, float maxTime, float nowPos, float maxPos) {
 		maxPos -= nowPos;
 		time /= maxTime;
 
@@ -30,14 +30,14 @@ public: //ÉÅÉìÉoä÷êî
 		return -maxPos * time * (time - 2.0f) + nowPos;
 	}
 
-	static inline float easeInBack(float time, float maxTime, float maxPos, float nowPos, float back = 1.70158f) {
+	static inline float easeInBack(float time, float maxTime, float nowPos, float maxPos, float back = 1.70158f) {
 		maxPos -= nowPos;
 		time /= maxTime;
 
 		return maxPos * time * time * ((back + 1) * time - back) + nowPos;
 	}
 
-	static inline float easeOutBack(float time, float maxTime, float maxPos, float nowPos, float back = 1.70158f) {
+	static inline float easeOutBack(float time, float maxTime, float nowPos, float maxPos, float back = 1.70158f) {
 		maxPos -= nowPos;
 		time = time / maxTime - 1;
 
@@ -48,24 +48,31 @@ public: //ÉÅÉìÉoä÷êî
 		const float n1 = 7.5625f;
 		const float d1 = 2.75f;
 
-		maxPos -= nowPos;
-		time /= maxTime - 1.0f;
+		if (time <= 0.0f) {
+			return nowPos;
+		}
 
-		if (time < 1.0f / d1) {
-			float t = time;
-			return maxPos * (n1 * t * t) + nowPos;
+		if (time >= maxTime) {
+			return maxPos;
 		}
-		else if (time < 2.0f / d1) {
-			float t = time - 1.5f / d1;
-			return maxPos * (n1 * t * t + 0.75f) + nowPos;
+
+		maxPos -= nowPos;
+		float timeRate = time / maxTime;
+
+		if (timeRate < (1.0f / 2.75f)) {
+			return nowPos * (1.0f - timeRate) + maxPos * timeRate;
 		}
-		else if (time < 2.5f / d1) {
-			float t = time - 2.25f / d1;
-			return maxPos * (n1 * t * t + 0.9375f) + nowPos;
+		else if (timeRate < (2.0f / 2.75f)) {
+			timeRate -= (1.5f / 2.75f);
+			return nowPos * (7.5625f * (timeRate) * timeRate + 0.75f) + maxPos;
+		}
+		else if (timeRate < (2.5f / 2.75f)) {
+			timeRate -= (2.25f / 2.75f);
+			return nowPos * (7.5625f * (timeRate) * timeRate + 0.9375f) + maxPos;
 		}
 		else {
-			float t = time - 2.625f / d1;
-			return maxPos * (n1 * t * t + 0.984375f) + nowPos;
+			timeRate -= (2.625f / 2.75f);
+			return nowPos * (7.5625f * (timeRate) * timeRate + 0.984375f) + maxPos;
 		}
 	}
 };
